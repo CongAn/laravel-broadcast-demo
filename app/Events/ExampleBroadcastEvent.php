9f9882ff3ce2aa4789c6a274584342ea0c7bd24e
Anker <anker.wang@slicejobs.com>
2021-04-07 12:58:11 +0800

    添加广播事件，设定通道名称 example-broadcast-channel

---
+++ app/Events/ExampleBroadcastEvent.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ExampleBroadcastEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('example-broadcast-channel');
+    }
+
+    /**
+     * 获取广播数据
+     *
+     * @return array
+     */
+    public function broadcastWith()
+    {
+        return [
+            'data' => 'content',
+            'date' => date('Y-m-d h:i:s A'),
+        ];
+    }
+}
